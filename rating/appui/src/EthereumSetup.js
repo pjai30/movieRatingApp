fcd9550a3e7e83ea465c97553699fee7ac1636b9
Parikshit <pari.jain.82@gmail.com>
2019-09-15 08:13:01 -0700

    add

--- rating/appui/src/EthereumSetup.js
+++ rating/appui/src/EthereumSetup.js
@@ -2,8 +2,8 @@ import Web3 from 'web3';
 
 const web3=new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 //const web3=new Web3(Web3.currentProvider);
-let ratingABI=[{"constant":true,"inputs":[{"name":"","type":"bytes32"}],"name":"ratingsReceived","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"movieList","outputs":[{"name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[{"name":"movieNames","type":"bytes32[]"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"constant":true,"inputs":[{"name":"movie","type":"bytes32"}],"name":"totalVotesFor","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"movie","type":"bytes32"}],"name":"voteForMovie","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"}]
-let ratingAddress='0x69560ae86f7a88a24e404e8fd0b62aaf2651fd2c';
+let ratingABI=[{"constant":true,"inputs":[{"name":"","type":"string"}],"name":"ratingsReceived","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"movieList","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[{"name":"movieNames","type":"string[]"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"constant":true,"inputs":[{"name":"movie","type":"string"}],"name":"totalVotesFor","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"movie","type":"string"}],"name":"voteForMovie","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"}]
+let ratingAddress='0x2d3798616746E3a6d95f78cb4d6B988cD8f25ccf';
 web3.eth.defaultAccount = web3.eth.accounts[0]
 
 
--- rating/contracts/Rating.sol
+++ rating/contracts/Rating.sol
@@ -1,32 +1,33 @@
-pragma solidity ^0.4.17;
+//pragma solidity ^0.5.8;
+pragma experimental ABIEncoderV2;
 // We have to specify what version of compiler this code will compile with
 
 contract Rating {
   /* mapping field below is equivalent to an associative array or hash.
   */
   
-  mapping (bytes32 => uint8) public ratingsReceived;
+  mapping (string => uint8) public ratingsReceived;
   
   /* We will use an array of bytes32 to store the list of movies
   */
   
-  bytes32[] public movieList;
+  string[] public movieList;
 
   /* This is the constructor which will be called once when you
   deploy the contract to the blockchain. When we deploy the contract,
   we will pass an array of movies for which users will give ratings
   */
-  function Rating(bytes32[] movieNames) public {
+  constructor(string[] memory movieNames) public {
     movieList = movieNames;
   }
 
   // This function returns the total ratings a movie has received so far
-  function totalVotesFor(bytes32 movie) view public returns (uint8) {
+  function totalVotesFor(string memory movie) view public returns (uint8) {
     return ratingsReceived[movie];
   }
 
   // This function increments the vote count for the specified movie. Equivalent to upvoting
-  function voteForMovie(bytes32 movie) public {
+  function voteForMovie(string memory movie) public {
     ratingsReceived[movie] += 1;
   }
 }
